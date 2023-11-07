1a6144585759faf9e9c496b8105d9eb44cea97a8
FishBallMEOW <94208393+FishBallMEOW@users.noreply.github.com>
2023-11-07 11:47:05 +0000

    add RUG_vPython modified Panda3D_Test modified vPython_Test

--- Code/vPython_Test.py
+++ Code/vPython_Test.py
@@ -1,5 +1,19 @@
 from vpython import *
 from time import *
+
+scene.width = 1500
+scene.height = 600
+
+def display_instructions():
+    s = """  To rotate "camera", drag with right button or Ctrl-drag.
+  To zoom, drag with middle button or Alt/Option depressed, or use scroll wheel.
+    On a two-button mouse, middle is left + right.
+  To pan left/right and up/down, Shift-drag.
+  Touch screen: pinch/extend to zoom, swipe or two-finger rotate."""
+    scene.caption = s
+
+display_instructions()
+
 plate=box(pos=vector(0,-5,0),color=color.white,length=20,width=20,height=0.1)
 ball=sphere(pos=vector(0,-5,0),color=color.yellow,radius=1)
 
