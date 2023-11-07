f2c7a5edfe77693df3c9047f186cd40878c3b1e2
FishBallMEOW <94208393+FishBallMEOW@users.noreply.github.com>
2023-11-07 05:25:41 +0000

    add some experiment codes for vPython_Test

---
+++ Code/vPython_Test.py
@@ -0,0 +1,7 @@
+from vpython import *
+from time import *
+plate=box(pos=vector(0,-5,0),color=color.white,length=20,width=20,height=0.1)
+ball=sphere(pos=vector(0,-5,0),color=color.yellow,radius=1)
+
+while True:
+    pass
